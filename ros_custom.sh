8027548371e1fb902ba5a9a677123b3707412aec
Gastd <gabriel.araujo.5000@gmail.com>
2018-08-09 00:01:52 -0300

    Add script

---
+++ ros_custom.sh
@@ -0,0 +1,12 @@
+# deal with tinyxml2
+# ln -s /usr/local/lib/libtinyxml.so.5.0 libtinyxml2.so.4
+
+# set enviroment
+_catkin_home=$(pwd)
+echo $_catkin_home
+export PATH="$_catkin_home/devel/:/opt/ros/kinetic/bin:$PATH"
+export CMAKE_PREFIX_PATH="$_catkin_home/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="$_catkin_home/devel/lib:/usr/local/lib:$LD_LIBRARY_PATH"
+export ROS_PACKAGE_PATH="$_catkin_home/devel/:$_catkin_home/src/:$ROS_PACKAGE_PATH"
+export PKG_CONFIG_PATH="$_catkin_home/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+unset _catkin_home
