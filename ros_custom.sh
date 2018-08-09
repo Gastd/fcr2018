174fefe0635bcea59f819ab6797d33e746bf1f82
Gastd <gabriel.araujo.5000@gmail.com>
2018-08-09 10:49:52 -0300

    modify enviroment

--- ros_custom.sh
+++ ros_custom.sh
@@ -5,7 +5,7 @@
 _catkin_home=$(pwd)
 echo $_catkin_home
 export PATH="$_catkin_home/devel/:/opt/ros/kinetic/bin:$PATH"
-export CMAKE_PREFIX_PATH="$_catkin_home/devel:$CMAKE_PREFIX_PATH"
+export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:$_catkin_home/devel"
 export LD_LIBRARY_PATH="$_catkin_home/devel/lib:/usr/local/lib:$LD_LIBRARY_PATH"
 export ROS_PACKAGE_PATH="$_catkin_home/devel/:$_catkin_home/src/:$ROS_PACKAGE_PATH"
 export PKG_CONFIG_PATH="$_catkin_home/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
